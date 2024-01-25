c40c469cc0c06def4f0549c5c57cda5e0759e09b
João Rodrigues <johnnyrod4569@gmail.com>
2024-01-25 12:42:47 +0100

    feat: Create GameBoard component with the basic structure of the Gameboard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,20 @@
+const initialGAmeBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+export default function GameBoard() {
+return (
+  <ol id="game-board">
+    {initialGAmeBoard.map((row, rowIndex) => <li key={rowIndex}>
+      <ol>
+        {row.map((playerSymbol, colIndex) => (
+        <li key={colIndex}>
+          <button>{playerSymbol}</button>
+        </li>))}
+      </ol>
+    </li>)}
+  </ol>
+);
+}
